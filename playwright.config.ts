62432d550f00a301ec0d81bf32652e4c4fa31e7c
Mercy Tum <mercytum@gmail.com>
2025-04-07 23:18:52 -0400

    updates playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'COVERAGE=true yarn web',
+    command: 'yarn web',
     url: 'http://localhost:19006',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 2 minutes
